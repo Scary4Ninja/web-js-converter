0513859d3ecc022c9308532f59780c818117f8db
Scary4Ninja <danchick_2001@mail.ru>
2022-09-21 10:18:41 +0200

    added button about creator

--- calc.js
+++ calc.js
@@ -22,26 +22,34 @@ function temperatureConverterCel(valNumCel) {
     let input = document.getElementById('inputFahrenheit');
     input.value = Math.floor(((valNumCel * 1.8) + 32) * 100) / 100;
     if (input.value > 122) {
-      input.value = 122
+      input.value = 122;
+      this.alert("Пожалуйста введите температуру в диапазоне от -80 до 50");
     } 
     else if (input.value < -112){
-      input.value = -112
+      input.value = -112;
+      this.alert("Пожалуйста введите температуру в диапазоне от -80 до 50");
     }
     let elem = document.getElementById('inputCelsius');
     if (valNumCel > 50) {
       elem.value = '50';
     }
     else if (valNumCel < -80) {
-      elem.value = '-80'
+      elem.value = '-80';
     }
   }
 
 function LengthConverter(valNum) {
+  valNum = parseFloat(valNum);
   let input = document.getElementById('inputMiles');
   input.value = Math.floor((valNum*0.62137)*100)/100;
 }
 
-function LengthConverterMil(valNum) {
+function LengthConverterMil(valNumKm) {
+  valNumKm = parseFloat(valNumKm);
   let input = document.getElementById('inputKilometers');
-  input.value = Math.floor((valNum/0.62137)*100)/100;
+  input.value = Math.floor((valNumKm/0.62137)*100)/100;
 }
+
+function AuthorsInfo() {
+  this.alert('Разработан студентами группы 19-ВТ-1:\nВасильев Данил\nВоробей Александр\nГригорьев Кирилл')
+  }
